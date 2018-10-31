6eafc1d75223735b0082e3f8b19087ba60a24a42
Connor Zanin <cnnrznn@gmail.com>
2018-10-31 09:19:31 -0400

    working on execution algorithm

--- entity.c
+++ entity.c
@@ -28,30 +28,43 @@ receive_op(operation op)
 void
 exec_op(void)
 {
-        operation *op, us;
+        int i, j;
+        operation *op, us, *lop=NULL;
         memcpy(us.s, state, COLLAB_MAX*sizeof(uint32_t));
 
         for (i=0; i<Queue->n; i++) {
                 op = Queue->arr[i];
 
                 if (comp_operation(op, &us) < 0)
-                        continue;
+                        continue;                       // 'future' operation (sending site has executed other ops that we haven't
                 if (0 == comp_operation(op, &us))
-                        goto exec;
+                        goto exec;                      // clocks are the same, i.e. op executed at the same logical time
 
-                // transform operation
+                // operation happened in the past, must tranform in order to execute now
+
+                // find most recent log entry where the clock is <= op's
                 for (j=Log->n-1; j>=0; j--) {
-                        lop = Log->arr[i];
-                        if (comp_operation(lop, op) >= 0)
+                        if (comp_operation(Log->arr[i], op) >= 0) {
+                                lop = Log->arr[i];
                                 break;
+                        }
                 }
-                for (; j<Log->n; j++) {
+
+                // do transformation
+                while (NULL != lop && NULLOP != op->o.type) {
                         // transform op
+                        if (op->s[lop->pid] <= lop->s[lop->pid])
+                                op->o = op_transform(op->o, lop->o, op->pid, lop->pid);
+
+                        // next Log op
+                        lop = (j < Log->n) ? Log->arr[++j] : NULL;
                 }
 
 exec:
                 // 1. remove operation from queue
                 q_remove(Queue, i);
+                i--;
+
                 op_perform(op);
                 memcpy(op->s, state, COLLAB_MAX*sizeof(uint32_t)); // op performed at OUR clock
                 q_push(Log, op);
