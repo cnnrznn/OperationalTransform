609d11796ca0f5999dfdf4040b04fd9fc427a7bd
Connor Zanin <cnnrznn@gmail.com>
2018-11-10 17:41:20 -0500

    client to randomly generate operations

--- core/mains/client.c
+++ core/mains/client.c
@@ -13,6 +13,22 @@
 static char cont = 1;
 
 static void
+do_input()
+{
+        operation *op;
+
+        if (rand()%1000 > 50)
+                return;
+
+        op = malloc(sizeof(operation));
+        op->type = (rand() % 2) + 1;
+        op->c = (rand() % 129) + 97; // a through }
+        op->pos = rand() % DOCKSIZE;
+
+        ot_client_put_user_op(op);
+}
+
+static void
 sigint_handler(int sig)
 {
         cont = 0;
@@ -23,30 +39,32 @@ int main(int argc, char **argv)
         operation op, *newop;
 
         signal(SIGINT, sigint_handler);
+        srand(time(NULL));
 
         ot_client_init();
         net_client_init();
 
-        // "instant user input"
-        while (EOF != scanf("%d,%c,%u", &op.type, &op.c, &op.pos)) {
-                newop = malloc(sizeof(operation));
-                memcpy(newop, &op, sizeof(operation));
+        //// "instant user input"
+        //while (EOF != scanf("%d,%c,%u", &op.type, &op.c, &op.pos)) {
+        //        newop = malloc(sizeof(operation));
+        //        memcpy(newop, &op, sizeof(operation));
 
-                ot_client_put_user_op(newop);
-        }
+        //        ot_client_put_user_op(newop);
+        //}
 
-        print_document(stderr);
+        //print_document(stderr);
 
         while (1) {
-                fprintf(stderr, "Client loop\n");
+                do_input();
+
+                //fprintf(stderr, "Client loop\n");
                 net_client_drain();
                 ot_client_drain();
 
                 print_document(stderr);
+
                 sleep(1);
         }
-
-        print_document(stdout);
 
         net_client_free();
         ot_client_free();
