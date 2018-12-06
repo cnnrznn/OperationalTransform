a1b4ef807878768f783c39c5cfc4c4963f64cce7
Connor Zanin <cnnrznn@gmail.com>
2018-12-06 00:33:37 -0500

    Use CMake for building ncurses client

---
+++ ncurses/user-input.c
@@ -0,0 +1,40 @@
+#include <ncurses.h>
+
+void
+get_document_name(WINDOW *win, char *buf, size_t size)
+{
+        int index = 0;
+        int ch;
+        int y, x, ymax, xmax;
+
+        getmaxyx(win, ymax, xmax);
+        getyx(win, y, x);
+        move(ymax-1, x);
+
+        printw("document name: ");
+
+        while (index < size-1) {
+                ch = getch();
+                if (10 == ch) { // ENTER key
+                        return;
+                }
+                else if (KEY_BACKSPACE == ch) {
+                        if (0 == index)
+                                continue;
+
+                        index--;
+                        buf[index] = 0;
+
+                        getyx(stdscr, y, x);
+                        mvdelch(y, x-1);
+                }
+                else if (65 <= ch && ch <= 122) {
+                        buf[index] = ch;
+                        index++;
+
+                        addch(ch);
+                }
+
+                refresh();
+        }
+}
---
+++ ncurses/user-input.h
@@ -0,0 +1,7 @@
+#ifndef _USER_INPUT_H
+#define _USER_INPUT_H
+
+void
+get_document_name(WINDOW *win, char *buf, size_t size);
+
+#endif /* _USER_INPUT_H */
