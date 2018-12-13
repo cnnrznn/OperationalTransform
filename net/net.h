c278b6231030e15b64b563e5636242a805a0ebe7
Connor Zanin <cnnrznn@gmail.com>
2018-12-13 09:16:26 -0500

    header file to match .c

--- net/net.h
+++ net/net.h
@@ -7,4 +7,10 @@ net_init(char *port);
 void
 net_fini(void);
 
+ssize_t
+net_recv(void *buf, size_t len);
+
+ssize_t
+net_sendto(void *buf, size_t len, char *host, char *port);
+
 #endif /* _NET_H */
